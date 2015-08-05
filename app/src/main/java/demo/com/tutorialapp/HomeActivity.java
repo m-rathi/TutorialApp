a281ceab855a29b71354cf9e5f7ce78793dc70f8
Mayuri <mayuri.rathi@ccr-tech.com>
2015-08-05 12:32:02 +0530

    google sign in and sign out

--- app/src/main/java/demo/com/tutorialapp/HomeActivity.java
+++ app/src/main/java/demo/com/tutorialapp/HomeActivity.java
@@ -1,113 +1,151 @@
 package demo.com.tutorialapp;
 
 import android.app.Activity;
-import android.content.Context;
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.content.IntentSender;
 import android.os.Bundle;
-import android.os.Looper;
-import android.support.v4.app.FragmentActivity;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.util.Log;
 import android.view.View;
-
+import android.widget.ImageButton;
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.SignInButton;
-import com.google.android.gms.common.api.Api;
 import com.google.android.gms.common.api.GoogleApiClient;
-import com.google.android.gms.common.api.PendingResult;
-import com.google.android.gms.common.api.Result;
 import com.google.android.gms.common.api.Scope;
-import com.google.android.gms.common.api.Status;
-import com.google.android.gms.common.api.zza;
-import com.google.android.gms.common.api.zzi;
 import com.google.android.gms.plus.Plus;
 
-import java.io.FileDescriptor;
-import java.io.PrintWriter;
-import java.util.concurrent.TimeUnit;
 
+public class HomeActivity extends Activity implements View.OnClickListener, GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
 
-public class HomeActivity extends Activity implements View.OnClickListener, GoogleApiClient.ConnectionCallbacks {
- private SignInButton sign_in_button;
+   //buttons sign in and sign out
+    private SignInButton sign_in_button;
+    private ImageButton sign_out_button;
+
     private GoogleApiClient googleApiClient;
+    private int signinprogress;
+    private PendingIntent signinintent;
+
+    private static final int SIGNED_IN = 0;
+    private static final int STATE_SIGNING_IN = 1;
+    private static final int STATE_IN_PROGRESS = 2;
+    private static final int RC_SIGN_IN = 0;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
-        sign_in_button=(SignInButton)findViewById(R.id.sign_in_button);
+        sign_in_button = (SignInButton) findViewById(R.id.sign_in_button);
+        sign_out_button = (ImageButton) findViewById(R.id.sign_out_button);
         sign_in_button.setOnClickListener(this);
-
-        googleApiClient=buildGoogleApiClient();
+        sign_out_button.setOnClickListener(this);
+        googleApiClient = buildGoogleApiClient();
     }
+
 
     private GoogleApiClient buildGoogleApiClient() {
 
         return new GoogleApiClient.Builder(this)
                 .addConnectionCallbacks(this)
                 .addOnConnectionFailedListener(this)
-                .addApi(Plus.API,Plus.PlusOptions.builder().build())
-                .addScope(new Scope("email")).build();}
+                .addApi(Plus.API, Plus.PlusOptions.builder().build())
+                .addScope(new Scope("email")).build();
+    }
 
-            @Override
-            public void registerConnectionCallbacks(GoogleApiClient.ConnectionCallbacks connectionCallbacks) {
 
-            }
+    protected void onStart() {
+        super.onStart();
+        googleApiClient.connect();
+    }
 
-            @Override
-            public boolean isConnectionCallbacksRegistered(GoogleApiClient.ConnectionCallbacks connectionCallbacks) {
-                return false;
-            }
 
-            @Override
-            public void unregisterConnectionCallbacks(GoogleApiClient.ConnectionCallbacks connectionCallbacks) {
+    protected void onStop() {
+        super.onStop();
+        googleApiClient.disconnect();
+    }
 
-            }
-
-            @Override
-            public void registerConnectionFailedListener(GoogleApiClient.OnConnectionFailedListener onConnectionFailedListener) {
-
-            }
-
-            @Override
-            public boolean isConnectionFailedListenerRegistered(GoogleApiClient.OnConnectionFailedListener onConnectionFailedListener) {
-                return false;
-            }
-
-            @Override
-            public void unregisterConnectionFailedListener(GoogleApiClient.OnConnectionFailedListener onConnectionFailedListener) {
-
-            }
-
-            @Override
-            public void dump(String s, FileDescriptor fileDescriptor, PrintWriter printWriter, String[] strings) {
-
-            }
-
-            @Override
-            public int getSessionId() {
-                return 0;
-            }
 
     @Override
     public void onConnected(Bundle bundle) {
-
+        Log.i("", "on connected ");
+        sign_in_button.setEnabled(false);
+        sign_out_button.setEnabled(true);
     }
 
     @Override
     public void onConnectionSuspended(int i) {
-
-    }
-
-    @Override
-    public void onClick(View view) {
-
-    }
-}
+        googleApiClient.connect();
+        signinprogress = SIGNED_IN;
     }
 
 
     @Override
-    public void onClick(View view) {
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+        if (signinprogress != STATE_IN_PROGRESS) {
+            signinintent = connectionResult.getResolution();
+            if (signinprogress == STATE_SIGNING_IN) {
+                resolveSignInError();
+            }
+        }
+        whenSignedOut();
+    }
 
+
+    private void resolveSignInError() {
+        if (signinintent != null) {
+            try {
+                signinprogress = STATE_IN_PROGRESS;
+                startIntentSenderForResult(signinintent.getIntentSender(),
+                        RC_SIGN_IN, null, 0, 0, 0);
+            } catch (IntentSender.SendIntentException e) {
+                signinprogress = STATE_SIGNING_IN;
+                googleApiClient.connect();
+            }
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        switch (requestCode) {
+            case RC_SIGN_IN:
+                if (resultCode == RESULT_OK) {
+                    signinprogress = STATE_SIGNING_IN;
+                } else {
+                    signinprogress = SIGNED_IN;
+                }
+
+                if (!googleApiClient.isConnecting()) {
+                    googleApiClient.connect();
+                }
+                break;
+        }
+    }
+
+    private void whenSignedOut() {
+        Log.i("", "when signed out");
+        sign_in_button.setEnabled(true);
+        sign_out_button.setEnabled(false);
+    }
+
+    @Override
+    public void onClick(View view) {
+        if (!googleApiClient.isConnecting()) {
+            switch (view.getId()) {
+                case R.id.sign_in_button:
+                    // Intent i=new Intent(getApplicationContext(),GcmActivity.class);
+                    // startActivity(i);
+                    // mStatus.setText("Signing In");
+                    Log.i("signed in ", "");
+                    resolveSignInError();
+                    break;
+
+                case R.id.sign_out_button:
+                    Log.i("signed out", "");
+                    Plus.AccountApi.clearDefaultAccount(googleApiClient);
+                    googleApiClient.disconnect();
+                    googleApiClient.connect();
+                    break;
+            }
+        }
     }
 }
